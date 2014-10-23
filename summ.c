e300908d8732a536ea170c1dca4d820d8adc2915
Smirnov Alexander <alllecs@yandex.ru>
2014-10-23 15:56:10 +0400

    Добавлено окончание программы если неправильно введен аргумент

--- summ.c
+++ summ.c
@@ -7,7 +7,8 @@ int main(int argc, char *argv[])
 	FILE *fp;
 
 	if (argc < 2) {
-		printf("\nНеправильно введен аргумент%d\n", argc);
+		printf("\nНеправильно введен аргумент = %d\n", argc);
+		return 3;
 	}
 	fp = fopen(argv[1], "r");
 
