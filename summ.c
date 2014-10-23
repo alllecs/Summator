216b95b4328e29923669dfdaa7983813817d626a
Smirnov Alexander <alllecs@yandex.ru>
2014-10-23 14:09:45 +0400

    Отформатирован текст программы

--- summ.c
+++ summ.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
-	int i = 0;
+	int f1, f2, f3, f4, f5, f6;
 	FILE *fp;
-	float f1, f2, f3, f4, f5, f6;
 
-	if (argc != 3)
-	for (i = 1; i < argc; i++)
+	fp = fopen(argv[1], "r");
 
-	fp = fopen(argv[i], "r");
 	if (fp == NULL ) {
-                printf("Ошибка открытия файла.\n");
-		return 0;
+                perror("fopen");
+		return 2;
 	}
-	fscanf(fp, "%f %f %f %f %f %f", &f1, &f2, &f3, &f4, &f5, &f6);
+	fscanf(fp, "%d %d %d %d %d %d", &f1, &f2, &f3, &f4, &f5, &f6);
 
 	fclose(fp);
 
-	printf("Сумма первых двух чисел:%.2f\n", f1 + f2);
-        printf("Сумма вторых двух чисел:%.2f\n", f3 + f4);
-        printf("Сумма третьих двух чисел:%.2f\n", f5 + f6);
+	printf("%.2d\n", f1 + f2);
+        printf("%.2d\n", f3 + f4);
+        printf("%.2d\n", f5 + f6);
 
 	return 0;
 }
