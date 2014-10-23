4edf20f3f892f2b6161c55aa0027dc3aad3ed585
Smirnov Alexander <alllecs@yandex.ru>
2014-10-23 12:25:39 +0400

    Исправлено perror на printf. Программа выдает ошибку открытия файла.

--- summ.c
+++ summ.c
@@ -12,12 +12,16 @@ int main(int argc, char * argv[])
 	for (i = 1; i < argc; i++)
 
 	fp = fopen(argv[i], "r");
+	if (fp == NULL ) {
+                printf("Ошибка открытия файла.\n");
+		return 0;
+	}
 	fscanf(fp, "%f %f %f %f %f %f", &f1, &f2, &f3, &f4, &f5, &f6);
 
-	if (fp == NULL ) {
-		perror("Ошибка открытия файла.\n");
-		exit(EXIT_FAILURE);
-	}
+//	if (fp == NULL ) {
+//		printf("Ошибка открытия файла.\n");
+//		exit(EXIT_FAILURE);
+//	}
 	fclose(fp);
 
 	printf("Сумма первых двух чисел:%.2f\n", f1 + f2);
