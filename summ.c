f3024952c9ec52bf5abb7b36f3e46596f02df198
Smirnov Alexander <alllecs@yandex.ru>
2014-10-23 13:20:49 +0400

    Удалены ненужные строки.

--- summ.c
+++ summ.c
@@ -5,7 +5,6 @@ int main(int argc, char * argv[])
 {
 	int i = 0;
 	FILE *fp;
-	char ch;
 	float f1, f2, f3, f4, f5, f6;
 
 	if (argc != 3)
@@ -18,10 +17,6 @@ int main(int argc, char * argv[])
 	}
 	fscanf(fp, "%f %f %f %f %f %f", &f1, &f2, &f3, &f4, &f5, &f6);
 
-//	if (fp == NULL ) {
-//		printf("Ошибка открытия файла.\n");
-//		exit(EXIT_FAILURE);
-//	}
 	fclose(fp);
 
 	printf("Сумма первых двух чисел:%.2f\n", f1 + f2);
