0b5cda619c2d7fc87eb3cf348813b63daa00a5dd
Smirnov Alexander <alllecs@yandex.ru>
2014-10-23 11:39:03 +0400

    Добавлены переменные. Файл суммирует первые 2 числа.

--- summ.c
+++ summ.c
@@ -6,21 +6,24 @@ int main(int argc, char * argv[])
 	int i = 0;
 	FILE *fp;
 	char ch;
+	float f1, f2;
 
 	if (argc != 3)
 	for (i = 1; i < argc; i++)
 
 	fp = fopen(argv[i], "r");
+	fscanf(fp, "%f %f", &f1, &f2);
 
 	if (fp == NULL ) {
 		perror("Ошибка открытия файла.\n");
 		exit(EXIT_FAILURE);
 	}
-	printf("В файле записано:\n", argv[i]);
-
-	while ( (ch = fgetc(fp)) != EOF )
-	printf("%c", ch);
-
 	fclose(fp);
+
+	printf("В файле записано:%.2f\n", f1 + f2);
+
+//	while ( (ch = fgetc(fp)) != EOF )
+//	printf("%c", ch);
+
 	return 0;
 }
