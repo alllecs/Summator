c613bdfeef07c4707cb6cf8fc2a7af86f294882f
Smirnov Alexander <alllecs@yandex.ru>
2014-10-23 11:50:35 +0400

    В программу добавлены константы. Убраны лишние строки.

--- summ.c
+++ summ.c
@@ -6,13 +6,13 @@ int main(int argc, char * argv[])
 	int i = 0;
 	FILE *fp;
 	char ch;
-	float f1, f2;
+	float f1, f2, f3, f4, f5, f6;
 
 	if (argc != 3)
 	for (i = 1; i < argc; i++)
 
 	fp = fopen(argv[i], "r");
-	fscanf(fp, "%f %f", &f1, &f2);
+	fscanf(fp, "%f %f %f %f %f %f", &f1, &f2, &f3, &f4, &f5, &f6);
 
 	if (fp == NULL ) {
 		perror("Ошибка открытия файла.\n");
@@ -20,10 +20,9 @@ int main(int argc, char * argv[])
 	}
 	fclose(fp);
 
-	printf("В файле записано:%.2f\n", f1 + f2);
-
-//	while ( (ch = fgetc(fp)) != EOF )
-//	printf("%c", ch);
+	printf("Сумма первых двух чисел:%.2f\n", f1 + f2);
+        printf("Сумма вторых двух чисел:%.2f\n", f3 + f4);
+        printf("Сумма третьих двух чисел:%.2f\n", f5 + f6);
 
 	return 0;
 }
