07c0ebbe0294d2a6dde35bf06032caf704c82523
Smirnov Alexander <alllecs@yandex.ru>
2014-10-28 20:08:41 +0400

    Добавлен цикл while

--- summ.c
+++ summ.c
@@ -3,26 +3,25 @@
 
 int main(int argc, char *argv[])
 {
-	int f1, f2, f3, f4, f5, f6;
+	int f1, f2, c;
 	FILE *fp;
 
 	if (argc > 2) {
 		printf("\nНеправильно введен аргумент = %d\n", argc);
 		return 3;
 	}
-	fp = fopen(argv[1], "r");
 
+	fp = fopen(argv[1], "r");
 	if (fp == NULL ) {
-                perror("error fopen");
+		perror("error fopen");
 		return 2;
 	}
-	fscanf(fp, "%d %d %d %d %d %d", &f1, &f2, &f3, &f4, &f5, &f6);
+
+	while (!feof(fp)) {
+		fscanf(fp,"%d %d", &f1, &f2);
+		printf("%.d\n", c = f1 + f2);
+	}
 
 	fclose(fp);
-
-	printf("%.d\n", f1 + f2);
-        printf("%.d\n", f3 + f4);
-        printf("%.d\n", f5 + f6);
-
 	return 0;
 }
